bd7e75465ae0cbe533c1af07070c6ae23eedd291
Raner Piibur <raner.piibur@gmail.com>
2025-08-02 21:35:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,6 +42,42 @@ create_symlink() {
     print_success "Linked $source -> $target"
 }
 
+# Function to install Nerd Fonts
+install_nerd_fonts() {
+    print_info "Installing Nerd Fonts (MesloLGS NF)..."
+    
+    # Create fonts directory
+    mkdir -p ~/.local/share/fonts
+    
+    # Download MesloLGS NF (recommended by Powerlevel10k)
+    cd /tmp
+    
+    fonts=(
+        "MesloLGS%20NF%20Regular.ttf"
+        "MesloLGS%20NF%20Bold.ttf"
+        "MesloLGS%20NF%20Italic.ttf"
+        "MesloLGS%20NF%20Bold%20Italic.ttf"
+    )
+    
+    for font in "${fonts[@]}"; do
+        if [ ! -f ~/.local/share/fonts/"${font//%20/ }" ]; then
+            print_info "Downloading ${font//%20/ }..."
+            wget -q "https://github.com/romkatv/powerlevel10k-media/raw/master/$font" -O ~/.local/share/fonts/"${font//%20/ }"
+        else
+            print_info "Font ${font//%20/ } already exists"
+        fi
+    done
+    
+    # Update font cache if available
+    if command -v fc-cache &> /dev/null; then
+        print_info "Updating font cache..."
+        fc-cache -fv > /dev/null 2>&1
+    fi
+    
+    print_success "Nerd Fonts installed"
+    print_info "Note: Configure your terminal to use 'MesloLGS NF' font for best experience"
+}
+
 # Create symlinks for dotfiles
 files=(".zshrc" ".p10k.zsh" ".gitconfig" ".aliases" ".functions" ".exports")
 
@@ -59,6 +95,9 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 else
     print_info "Oh My Zsh already installed"
 fi
+
+# Install Nerd Fonts
+install_nerd_fonts
 
 # Install Powerlevel10k theme
 if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
